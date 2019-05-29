fd01423e30aeaf17aad1f9842a2927e05a18afdc
Sadi Jr <sadijacinto@gmail.com>
2019-05-28 21:41:33 -0300

    Tentativa número 2

---
+++ src/main.c
@@ -0,0 +1,238 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ncurses.h>
+#include <pthread.h>
+
+
+#define MAX_THREADS 5
+#define LINES 11
+#define COLLUMNS 11
+#define CURSOR  1
+#define THREAD  2
+#define BLANK   3
+
+void start_game();
+void main_menu();
+
+bool winner, time_out, game_running;
+unsigned int max_time;
+unsigned long speed;
+int killed_threads = 0;
+
+typedef struct Ball {
+    int x;
+    int y;
+    bool alive;
+} ball;
+
+ball positions[MAX_THREADS];
+ball cursor;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t threads[MAX_THREADS];
+pthread_t cron;
+
+int main() {
+    srand(time(NULL));
+    initscr();
+	cbreak();
+	keypad(stdscr, TRUE);
+    noecho();
+    
+    if (has_colors() == FALSE) {
+        endwin();
+        printf("Seu terminal nao suporta cor\n");
+        exit(1);
+    }
+
+    start_color();
+    init_pair(CURSOR, COLOR_YELLOW, COLOR_YELLOW);
+    init_pair(THREAD, COLOR_RED, COLOR_RED);
+    init_pair(BLANK, COLOR_BLACK, COLOR_BLACK);
+
+    main_menu();
+    endwin();
+    exit(0);
+}
+
+void main_menu() {
+    clear();
+    mvprintw(0, 0, "Escolha uma dificuldade:");
+    mvprintw(1, 0, "(1) Facil");
+    mvprintw(2, 0, "(2) Medio");
+    mvprintw(3, 0, "(3) Dificil");
+    mvprintw(4, 0, "(q) Sair");
+
+    switch (getch()) {
+    case '1':
+        max_time = 120;
+        speed = 2.0;
+        start_game();
+        break;
+    
+    case '2':
+        max_time = 60;
+        speed = 1.0;
+        start_game();
+        break;
+    case '3':
+        max_time = 30;
+        speed = 0.5;
+        start_game();
+        break;
+
+    case 'q':
+    case 'Q':
+        endwin();
+        exit(0);
+    default:
+        main_menu();
+        break;
+    }
+}
+
+void start_game() {
+    game_running = true;
+    winner = false;
+    time_out = false;
+    clear_line(0); clear_line(1);
+    clear_line(2); clear_line(3);
+    clear_line(4);
+
+    init_positions();
+    init_table();
+    init_cursor();
+
+    for(int i = 0; i < MAX_THREADS; i++) {
+        pthread_create(&threads[i], NULL, move_thread(i), NULL);
+    }
+
+    pthread_create(&cron, NULL, create_timer(), NULL);
+    pthread_join(&cron, NULL);
+
+    for(int i = 0; i < MAX_THREADS; i++) {
+        pthread_join(&threads[i], NULL);
+    }
+
+    move_cursor();
+}
+
+void init_positions() {
+    for(int i = 0; i < MAX_THREADS + 1; i++) {
+        positions[i].alive = true;
+    }
+}
+
+void move_cursor() {
+    int key;
+    do {
+        key = getch();
+        switch (key) {
+        case KEY_UP:
+        case 'w':
+        case 'W':
+            if ((cursor.y > 0)) {
+            cursor.y = cursor.y - 1;
+            }
+            break;
+        case KEY_DOWN:
+        case 's':
+        case 'S':
+            if ((cursor.y < LINES - 1)) {
+            cursor.y = cursor.y + 1;
+            }
+            break;
+        case KEY_LEFT:
+        case 'a':
+        case 'A':
+            if ((cursor.x > 0)) {
+            cursor.x = cursor.x - 1;
+            }
+            break;
+        case KEY_RIGHT:
+        case 'd':
+        case 'D':
+            if ((cursor.x < COLS - 1)) {
+            cursor.x = cursor.x + 1;
+            }
+            break;
+        }
+  }while ((key != 'q') && (key != 'Q'));
+}
+
+void init_table() {
+    for(int i = 0; i < LINES; i++) {
+        for(int j = 0; j < COLLUMNS; j++) {
+            attron(COLOR_PAIR(BLANK));
+            mvaddch(j, i, ' ');
+            attroff(COLOR_PAIR(BLANK));
+        }
+    }
+}
+
+void refresh_table(int old_x, int old_y, int new_x, int new_y, bool thread) {
+    attron(COLOR_PAIR(BLANK));
+    mvaddch(old_x, old_y, ' ');
+    attroff(COLOR_PAIR(BLANK));
+
+    if(thread) {
+        attron(COLOR_PAIR(THREAD));
+        mvaddch(new_x, new_y, BLANK);
+        attroff(COLOR_PAIR(THREAD));
+    } else {
+        move(new_y, new_x);
+        refresh();
+        attron(COLOR_PAIR(CURSOR));
+        mvaddch(new_y, new_x, BLANK);
+        attroff(COLOR_PAIR(CURSOR));
+    }
+}
+
+void clear_line(int x) {
+    move(x, 0);          // move o cursor pro começo da linha
+    clrtoeol();          // deleta a linha
+}
+
+void* create_timer() {
+    for(int i = 0; i <= max_time; i++) {
+        clear_line(15); 
+        mvprintw(15, 0, "Tempo restante: %i", max_time - i);
+        sleep(1);
+        refresh();
+    }
+    time_out = true;
+    game_running = false;
+}
+
+void* move_thread(int id) {
+    while (game_running && !time_out && !winner && positions[id].alive) {
+        pthread_mutex_lock(&mutex);
+        int new_x, new_y;
+        do {
+            new_x = rand() % LINES;
+            new_y = rand() % COLLUMNS;
+        } while(!verify_free_position);
+        int old_x = positions[id].x;
+        int old_y = positions[id].y;
+        positions[id].x = new_x;
+        positions[id].y = new_y;
+        refresh_table(old_x, old_y, new_x, new_y, true);
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+bool verify_free_position(int new_x, int new_y) {
+    for(int i = 0; i <= MAX_THREADS; i++) {
+        if(positions[i].x == new_x && positions[i].y == new_y 
+            || cursor.x == new_x && cursor.y == new_y) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void create_cursor() {
+    cursor.x = 6;
+    cursor.y = 6;
+    refresh_table(0,0,6,6,false);
+}
